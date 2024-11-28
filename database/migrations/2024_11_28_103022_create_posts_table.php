de3926bd7d37ffd206b4dcda32a75c5c36542bd0
mktcode <kontakt@markus-kottlaender.de>
2024-11-28 11:52:37 +0100

    feat: change body column type from string to text in posts table migration

--- database/migrations/2024_11_28_103022_create_posts_table.php
+++ database/migrations/2024_11_28_103022_create_posts_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('body');
+            $table->text('body');
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
