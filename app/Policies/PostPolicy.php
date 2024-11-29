3d150ed414c38309de02952616df7d26a2c5f81e
mktcode <kontakt@markus-kottlaender.de>
2024-11-29 11:50:52 +0100

    feat: allow posting only every 5 minutes

--- app/Policies/PostPolicy.php
+++ app/Policies/PostPolicy.php
@@ -8,22 +8,6 @@ use App\Models\User;
 class PostPolicy
 {
     /**
-     * Determine whether the user can view any models.
-     */
-    public function viewAny(User $user): bool
-    {
-        return true;
-    }
-
-    /**
-     * Determine whether the user can view the model.
-     */
-    public function view(User $user, Post $post): bool
-    {
-        return true;
-    }
-
-    /**
      * Determine whether the user can create models.
      * Create a new post is only allowed every 5 minutes.
      */
@@ -44,22 +28,6 @@ class PostPolicy
      * Determine whether the user can delete the model.
      */
     public function delete(User $user, Post $post): bool
-    {
-        return $user->id === $post->user_id;
-    }
-
-    /**
-     * Determine whether the user can restore the model.
-     */
-    public function restore(User $user, Post $post): bool
-    {
-        return $user->id === $post->user_id;
-    }
-
-    /**
-     * Determine whether the user can permanently delete the model.
-     */
-    public function forceDelete(User $user, Post $post): bool
     {
         return $user->id === $post->user_id;
     }
--- resources/views/livewire/posts/create-post-form.blade.php
+++ resources/views/livewire/posts/create-post-form.blade.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Livewire\Volt\Component;
 
 new class extends Component {
@@ -7,6 +8,11 @@ new class extends Component {
 
     public function createPost(): void
     {
+        if (!auth()->user()->can('create', Post::class)) {
+            $this->addError('body', __('You can create a new post only once every minute.'));
+            return;
+        }
+
         $this->validate([
             'body' => 'required|string|max:255',
         ]);
@@ -23,9 +29,9 @@ new class extends Component {
 
 <div>
     <form wire:submit="createPost">
-        <x-input-label for="body" :value="__('Post')" />
-        <x-textarea-input wire:model="body" id="body" class="block mt-1 w-full" name="body" required autofocus />
-        <x-input-error :messages="$errors->get('body')" class="mt-2" />
+        <x-input-label for="body" :value="__('Post')"/>
+        <x-textarea-input wire:model="body" id="body" class="block mt-1 w-full" name="body" required autofocus/>
+        <x-input-error :messages="$errors->get('body')" class="mt-2"/>
 
         <x-primary-button class="mt-3">
             {{ __('Post') }}
